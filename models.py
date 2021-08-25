b21dd2399243b893ee94723d2d001e7c195a7fad
GreatDrake <greatdraken@gmail.com>
2021-08-25 19:07:37 +0300

    Add another CNN

--- models.py
+++ models.py
@@ -53,3 +53,23 @@ class CNN(nn.Module):
         x = nn.Dense(features=10)(x)    # There are 10 classes in MNIST
         return x
 
+class CNNSimple(nn.Module):
+    @nn.compact
+    # Provide a constructor to register a new parameter 
+    # and return its initial value
+    def __call__(self, x):
+        x = nn.Conv(features=32, kernel_size=(3, 3))(x)
+        x = nn.relu(x)
+        x = nn.avg_pool(x, window_shape=(2, 2), strides=(2, 2))
+    
+        x = nn.Dropout(0.1)(x, deterministic=True)
+
+        x = x.reshape((x.shape[0], -1)) # Flatten
+        x = nn.Dense(features=64)(x)
+        x = nn.relu(x)
+
+        x = nn.Dropout(0.2)(x, deterministic=True)    
+
+        x = nn.Dense(features=10)(x)    # There are 10 classes in MNIST
+        return x
+
